e6ee939e50ec98f600add8f37a1808303bd0782a
germanmaximilianomoreno <74334666+germanmaximilianomoreno@users.noreply.github.com>
2020-11-12 00:32:32 -0300

    v1

---
+++ menu.js
@@ -0,0 +1,69 @@
+/*Menu*/
+
+$(document).ready(main)
+
+var contador = 1;
+
+function main(){
+    $('.menu_bar').click(function(){
+       
+    if(contador == 1){
+           $('nav').animate({
+           left: '0'
+           });
+           contador = 0;
+       } else {
+           contador = 1;
+           $('nav').animate({
+            left: '-100%'
+        });
+
+       }
+
+   });
+
+};
+
+/*Botón de subir*/
+
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+    var currentScroll = document.documentElement.scrollTop;
+
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll / 10));
+    }
+}
+
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 500){
+        buttonUp.style.transform = "scale(1)";
+    } else if(scroll < 500){
+        buttonUp.style.transform = "scale(0)";
+    }
+}
+
+const colorSwitch = document.querySelector('.theme-switch input[type="Checkbox"]');
+
+function switchColor(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        document.documentElement.style.setProperty('--sombra', '0.62rem 0.62rem 0rem 0rem #46A3FB, 1.25rem 1.25rem #83C1FB');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        document.documentElement.style.setProperty('--sombra', '0.62rem 0.62rem 0rem 0rem #46A3FB, 1.25rem 1.25rem #83C1FB, 1.87rem 1.87rem 0.87rem #ACD5FD');
+    }
+}
+
+colorSwitch.addEventListener('change', switchColor, false);
+
+
+
