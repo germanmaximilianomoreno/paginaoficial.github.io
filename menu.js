797388eb3571510f432d0d8d4213fe51edab0b18
germanmaximilianomoreno <74334666+germanmaximilianomoreno@users.noreply.github.com>
2021-05-29 01:27:24 -0300

    v1

--- menu.js
+++ menu.js
@@ -56,10 +56,10 @@ const colorSwitch = document.querySelector('.theme-switch input[type="Checkbox"]
 function switchColor(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        document.documentElement.style.setProperty('--sombra', '0.62rem 0.62rem 0rem 0rem #46A3FB, 1.25rem 1.25rem #83C1FB');
+        document.documentElement.style.setProperty('--sombra', '0.62rem 0.62rem 0rem 0rem #369e61, 1.25rem 1.25rem #6fc290');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
-        document.documentElement.style.setProperty('--sombra', '0.62rem 0.62rem 0rem 0rem #46A3FB, 1.25rem 1.25rem #83C1FB, 1.87rem 1.87rem 0.87rem #ACD5FD');
+        document.documentElement.style.setProperty('--sombra', '0.62rem 0.62rem 0rem 0rem #369e61, 1.25rem 1.25rem #6fc290, 1.87rem 1.87rem 0.87rem #8cebb3');
     }
 }
 
