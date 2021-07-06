357af4a8e393c3b587d79e7d60eb5855f260e2e9
germanmaximilianomoreno <74334666+germanmaximilianomoreno@users.noreply.github.com>
2021-07-06 03:16:06 -0300

    v1

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,3 @@
-
 /*Menu*/
 
 $(document).ready(main)
@@ -60,24 +59,6 @@ window.onscroll = function(){
         buttonUp.style.transform = "scale(0)";
     }
 }
-
-
-/*Modo oscuro*/
-
-/*const colorSwitch = document.querySelector('.theme-switch input[type="Checkbox"]');
-
-function switchColor(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        document.documentElement.style.setProperty('--sombra', '0.62rem 0.62rem 0rem 0rem #369e61, 1.25rem 1.25rem #6fc290');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        document.documentElement.style.setProperty('--sombra', '0.62rem 0.62rem 0rem 0rem #369e61, 1.25rem 1.25rem #6fc290, 1.87rem 1.87rem 0.87rem #8cebb3');
-    }
-}
-
-colorSwitch.addEventListener('change', switchColor, false);*/
-
 
 
 
@@ -208,5 +189,144 @@ window.addEventListener('load', function(){
 
 
 
+/*Carrito de compras*/
+
+const Clickbutton = document.querySelectorAll('.button')
+const tbody = document.querySelector('.tbody')
+let carrito = []
+
+Clickbutton.forEach(btn => {
+    btn.addEventListener('click', addToCarritoItem)
+})
+
+function addToCarritoItem(e){
+    const button = e.target
+    const item = button.closest('.card')
+    const itemTitle = item.querySelector('.card-title').textContent;
+    const itemPrecio = item.querySelector('.precio').textContent;
+    const itemImg = item.querySelector('.card-img-top').src;
+
+    const newItem = {
+        title: itemTitle,
+        precio: itemPrecio,
+        img: itemImg,
+        cantidad: 1,
+    }
+
+    addItemCarrito(newItem)
+}
 
 
+function addItemCarrito(newItem){
+
+    const alert = document.querySelector('.alert')
+
+    setTimeout(function(){
+        alert.classList.add('hide')
+    }, 2000)
+       alert.classList.remove('hide')
+
+    const InputElemento = tbody.getElementsByClassName('input__elemento')
+
+    for(let i = 0; i < carrito.length; i++) {
+        if(carrito[i].title.trim() === newItem.title.trim()){
+            carrito[i].cantidad ++;
+            const inputValue = InputElemento[i]
+            inputValue.value++;
+            CarritoTotal()
+            return null;
+        }
+    }
+
+    carrito.push(newItem)
+
+    renderCarrito()
+}
+
+function renderCarrito(){
+    tbody.innerHTML = ''
+    carrito.map(item => {
+        const tr = document.createElement('tr')
+        tr.classList.add('ItemCarrito')
+        const Content = `
+        
+        <th scope="row"></th>
+                         <td class="table__productos">
+                            <img src=${item.img} alt="">
+                            <h5 class="title">${item.title}</h5>
+                         </td>
+                         <td class="table__precio"><p>${item.precio}</p></td>
+                         <td class="table__cantidad">
+                            <input type="number" min="1" value=${item.cantidad} class="input__elemento">
+                            <button class="delete btn btn-danger">x</button>
+                         </td>
+        
+        `
+
+        tr.innerHTML = Content;
+        tbody.append(tr)
+
+        tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
+        tr.querySelector(".input__elemento").addEventListener('change', sumaCantidad)
+    })
+    CarritoTotal()
+}
+
+function CarritoTotal(){
+    let Total = 0;
+    const itemCartTotal = document.querySelector('.itemCartTotal')
+    carrito.forEach((item) => {
+        const precio = Number(item.precio.replace("$", ''))
+        Total = Total + precio*item.cantidad
+    })
+
+    itemCartTotal.innerHTML = `Total $${Total}`
+    addLocalStorage()
+}
+
+function removeItemCarrito(e){
+    const buttonDelete = e.target
+    const tr = buttonDelete.closest(".ItemCarrito")
+    const title = tr.querySelector('.title').textContent;
+    for(let i = 0; i < carrito.length; i++) {
+
+        if(carrito[i].title.trim() === title.trim()) {
+            carrito.splice(i, 1)
+        }
+    }
+
+    const alert = document.querySelector('.remove')
+
+    setTimeout(function(){
+        alert.classList.add('remove')
+    }, 2000)
+       alert.classList.remove('remove')
+
+    tr.remove()
+    CarritoTotal()
+}
+
+function sumaCantidad(e) {
+    const sumaInput = e.target
+    const tr = sumaInput.closest(".ItemCarrito")
+    const title = tr.querySelector('.title').textContent;
+    carrito.forEach(item => {
+        if(item.title.trim() === title) {
+            sumaInput.value < 1 ? (sumaInput.value = 1) : sumaInput.value;
+            item.cantidad = sumaInput.value;
+            CarritoTotal()
+        }
+    })
+}
+
+function addLocalStorage() {
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+window.onload = function(){
+    const storage = JSON.parse(localStorage.getItem('carrito'));
+    if(storage) {
+        carrito = storage;
+        renderCarrito()
+    }
+}
